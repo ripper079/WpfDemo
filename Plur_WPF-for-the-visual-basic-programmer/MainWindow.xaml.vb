af6e9d27ea339a32aab65cbcf0d670115e5e9cbc
Daniel Oikarainen <powerdeveloper@fastmail.se>
2023-01-01 21:14:18 +0100

    Dynamically adding a user contol

--- Plur_WPF-for-the-visual-basic-programmer/MainWindow.xaml.vb
+++ Plur_WPF-for-the-visual-basic-programmer/MainWindow.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Class MainWindow
 
+    Private Sub menuitemProducts_Click(sender As Object, e As RoutedEventArgs)
+        contentArea.Children.Clear()
+        'Adding the user control dynamically
+        contentArea.Children.Add(New ProductDetailControl())
+    End Sub
 End Class
