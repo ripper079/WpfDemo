355233cec1bb59f05de944d01ecaaa2ae3f7a7fa
Daniel Oikarainen <powerdeveloper@fastmail.se>
2023-01-10 22:24:53 +0100

    DataTemplates and ContentPresenter added

---
+++ WpfDataTemplateANDContentPresenter/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Public Property myPerson As Person
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        myPerson = New Person() With
+        {
+            .Name = "Daniel Oikarainen",
+            .Age = 43,
+            .hasChildren = True
+        }
+
+        Me.DataContext = Me
+    End Sub
+End Class
---
+++ WpfDataTemplateANDContentPresenter/Models/Person.vb
@@ -0,0 +1,10 @@
+﻿Public Class Person
+    Public Property Name As String
+    Public Property Age As Integer
+    Public Property hasChildren As Boolean
+
+    Sub New()
+
+    End Sub
+
+End Class
